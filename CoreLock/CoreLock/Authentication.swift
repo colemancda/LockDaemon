9c251346c36070572f60c1bcd4cf24b68d0ebdd8
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-22 14:36:10 -0500

    made generate authentication token a protocol extension

--- CoreLock/CoreLock/Authentication.swift
+++ CoreLock/CoreLock/Authentication.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2015 ColemanCDA. All rights reserved.
 //
 
-
-// MARK: - Functions
-
-/// Generates the authorization header used for authenticating HTTP requests.
-/// Modeled after [AWS](http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#UsingTemporarySecurityCredentials)
-///
-/// - Parameter identifier: The identifier (e.g. resource ID or username) of the entity trying to authenticate.
-/// - Parameter secret: The secret (e.g. password) of the entity trying to authenticate.
-/// - Parameter context: The authentication context info.
-/// - Returns: The generated authentication token.
-public func GenerateAuthenticationToken(identifier: String, secret: String, context: AuthenticationContext) -> String {
+public extension AuthenticationContext {
     
-    return ""
+    /// Generates the authentication token used for authenticating requests.
+    /// Modeled after [AWS](http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#UsingTemporarySecurityCredentials)
+    ///
+    /// - Parameter identifier: The identifier (e.g. resource ID or username) of the entity trying to authenticate.
+    /// - Parameter secret: The secret (e.g. password) of the entity trying to authenticate.
+    /// - Parameter context: The authentication context info.
+    /// - Returns: The generated authentication token.
+    func generateToken(identifier: String, secret: String) -> String {
+        
+        return ""
+    }
 }
