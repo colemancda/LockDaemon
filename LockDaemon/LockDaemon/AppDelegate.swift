b99b75f96dce34060daf22fc75eae9a4f942a308
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-22 14:36:21 -0500

    created lock daemon app

---
+++ LockDaemon/LockDaemon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LockDaemon
+//
+//  Created by Alsey Coleman Miller on 6/22/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LockDaemon/LockDaemon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LockDaemon
+//
+//  Created by Alsey Coleman Miller on 6/22/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
